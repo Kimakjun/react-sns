ebafbcec7662766cfd9107cc7eff601991fc0ced
kimhakjun <dkd7142@naver.com>
2020-10-21 15:11:01 +0900

    반응형 디자인 적용 1. antd row, col 사용하여 반응형 디자인 적용

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import {Menu} from 'antd'
+import {Menu, Input, Row, Col} from 'antd'
+
 const AppLayout = ({children}) => {
     return (
         <div>
@@ -11,11 +12,22 @@ const AppLayout = ({children}) => {
                 <Menu.Item>
                     <Link href="/profile">프로필</Link>
                 </Menu.Item>
+                <Input.Search placeholder="input search text"  style={{ width: 200, verticalAlign: 'middle'}} />
                 <Menu.Item>
                     <Link href="/signup">회원가입</Link>
                 </Menu.Item>
             </Menu>
-            {children}
+            <Row gutter={8}>
+                <Col xs={24} md={6}>
+                    왼쪽메뉴
+                </Col>
+                <Col xs={24} md={12}>
+                    {children}
+                </Col>
+                <Col xs={24} md={6}>
+                    <a href="https://codingjuny.tistory.com/" target='_blank'>My blog</a>
+                </Col>
+            </Row>
         </div>
     );
 }
