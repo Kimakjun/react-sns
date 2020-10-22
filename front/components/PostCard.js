7e27bfce0b00c2c2082d3fac189e44aeb46d263b
kimhakjun <dkd7142@naver.com>
2020-10-23 01:35:25 +0900

    해쉬태그  링크 적용 1. 게시글 컨텐트에서 정규식으로 해시태그추출후 링크 태그 달아줌

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import PostImages from './PostImages';
 import CommentForm from './CommentForm';
+import PostCardContent from './PostCardContent';
 
 const PostCard = ({post}) => {
     const id = useSelector((state)=> state.user.me?.id);
@@ -49,7 +50,7 @@ const PostCard = ({post}) => {
             <Card.Meta
                 avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
                 title={post.User.nickname}
-                description={post.content}
+                description={<PostCardContent postData={post.content} />}
             />
             </Card>
             {commentFormOpend && (
