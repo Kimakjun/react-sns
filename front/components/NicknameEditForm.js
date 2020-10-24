83358f39f569ad32dea78d48ce37025f62c2c9b1
kimhakjun <dkd7142@naver.com>
2020-10-25 01:27:26 +0900

    nicknameEdit 컴포넌트 작성 1. nickname 변경시 닉네임 변경 액션 dispatch 2. 옵셔널 체이닝으로 me?.nickname me 없을경우 보호

--- front/components/NicknameEditForm.js
+++ front/components/NicknameEditForm.js
@@ -1,10 +1,23 @@
-import {useMemo} from 'react';
+import {useCallback, useMemo} from 'react';
 import {Form, Input} from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { CHANGE_NICKNAME_REQUEST } from '../reducers/user';
 
 const NicknameEditForm = () => {
     const style = useMemo(() => ({marginBottom: '20px', border: '1px solid #d9d9d9', padding: '20px'}), []);
+    const { me } = useSelector((state) => state.user);
+    const [nickname, onChangeNickName] = useInput(me?.nickname || '');
+    const dispatch = useDispatch();
+
+    const onSubmit = useCallback(() => {
+        dispatch({
+            type: CHANGE_NICKNAME_REQUEST,
+            data: nickname,
+        })
+    }, [nickname]);
+
     return (
-        <Form style={style}>
+        <Form style={style} onFinish={onSubmit}>
             <Input.Search addonBefore='닉네임' enterButton='수정'/>
         </Form>
     )
