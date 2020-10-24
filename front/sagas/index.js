9b73fce7eb403e032daf2d3397c5fa94962521df
kimhakjun <dkd7142@naver.com>
2020-10-25 00:24:48 +0900

    user saga 작성. 1. saga effect 활용(delay) 서버 비동기 요청 흉내 2. takeLatest 이용하여 action 감지

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -1,8 +1,11 @@
-import {all, fork,} from 'redux-saga';
+import {all, fork,} from 'redux-saga/effects';
+import postSaga from './post';
+import userSaga from './user';
 
 
 export default function* rootSaga(){
     yield all([ // all 은 배열안 모두실행.
-
+        fork(postSaga),
+        fork(userSaga),
     ])
 }
